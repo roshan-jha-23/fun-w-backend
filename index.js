6147d24d04b63ec46f34aa2c052c70917f78813e
roshan2345 <jha062305@gmail.com>
2024-03-07 00:29:28 +0530

    ok

--- index.js
+++ index.js
@@ -2,6 +2,44 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const githubData = {
+  login: "roshan-jha-23",
+  id: 141396860,
+  node_id: "U_kgDOCG2LfA",
+  avatar_url: "https://avatars.githubusercontent.com/u/141396860?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/roshan-jha-23",
+  html_url: "https://github.com/roshan-jha-23",
+  followers_url: "https://api.github.com/users/roshan-jha-23/followers",
+  following_url:
+    "https://api.github.com/users/roshan-jha-23/following{/other_user}",
+  gists_url: "https://api.github.com/users/roshan-jha-23/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/roshan-jha-23/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/roshan-jha-23/subscriptions",
+  organizations_url: "https://api.github.com/users/roshan-jha-23/orgs",
+  repos_url: "https://api.github.com/users/roshan-jha-23/repos",
+  events_url: "https://api.github.com/users/roshan-jha-23/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/roshan-jha-23/received_events",
+  type: "User",
+  site_admin: false,
+  name: "Roshan jha",
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: "Result Driven Engineer .",
+  twitter_username: "roshan_jha_23",
+  public_repos: 64,
+  public_gists: 0,
+  followers: 0,
+  following: 5,
+  created_at: "2023-08-04T06:57:29Z",
+  updated_at: "2024-02-17T19:19:43Z",
+};
+
 app.get("/", (req, res) => {
   res.send("welcome to the home page");
 });
@@ -16,6 +54,9 @@ app.get("/twitter", (req, res) => {
 
 app.get("/linkedin", (req, res) => {
   res.send("<h1>mera linkedin roshan ka</h1>");
+});
+app.get("/github", (req, res) => {
+  res.json(githubData)
 });
 
 app.listen(PORT, () => {
